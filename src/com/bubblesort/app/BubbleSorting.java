4e681f88e1871e38eb0099ef15e157e3c3b6e799
snakeice45 <jonas171192@gmail.com>
2019-10-16 08:18:45 -0500

    first commit of java bubble sort

---
+++ src/com/bubblesort/app/BubbleSorting.java
@@ -0,0 +1,75 @@
+package com.bubblesort.app;
+
+
+
+public class BubbleSorting {
+
+	public static void main(String[] args) {
+		//Constants declaration
+				final int ARRAY_LENGHT = 100;
+				
+				//variables declaration
+				boolean bubbleSwap = true;
+				int auxiliar = 0;
+				int count = 0;
+				int count2 = 0;
+				int count3 = 0;
+				
+				//array declaration
+				int[] arrayOne = new int[ARRAY_LENGHT];
+				
+				//object declaration
+				RandomNumbers randomNumbers = new RandomNumbers();
+				
+				//array seed
+				arrayOne = randomNumbers.InsertRandomNumbersTo(arrayOne, 101);
+				
+				//show original order
+				System.out.println("Original order bubble sort:");
+				for (int i = 0; i < ARRAY_LENGHT; i++) {
+					System.out.print(arrayOne[i] + " ");
+				}
+				System.out.println("");
+				
+				//process
+				while(bubbleSwap)
+				{
+					
+					if(count + 1 >= ARRAY_LENGHT)
+					{
+						count = 0;
+					}
+					
+					if(arrayOne[count] > arrayOne[count+1])
+					{
+						auxiliar = arrayOne[count+1];
+						arrayOne[count+1] = arrayOne[count];
+						arrayOne[count] = auxiliar;
+						count2 = 0;
+					}
+					if(arrayOne[count] < arrayOne[count+1])
+					{
+						count2++;
+						if(count2 == ARRAY_LENGHT)
+						{
+							bubbleSwap = false;
+							break;
+						}
+					}
+					
+					count3++;
+					count++;
+				}
+				System.out.println("Bubble Ordered:");
+				for (int i = 0; i < ARRAY_LENGHT; i++) {
+					System.out.print(arrayOne[i] + " ");
+				}
+				System.out.println("");
+				System.out.println("loops count " + count3);
+				
+				
+
+
+	}
+
+}
